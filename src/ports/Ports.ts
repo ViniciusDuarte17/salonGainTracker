72de6dc726de3a3a99b797a8e1ccd788528e30c1
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2023-08-15 20:30:47 -0300

    feat: configuração do ambiente

---
+++ src/ports/Ports.ts
@@ -0,0 +1,13 @@
+import { AuthenticationData } from "../services/Authenticator";
+
+export interface IHashManger {
+  hash(text: string): Promise<string>;
+  compare(text: string, hash: string): Promise<boolean>;
+}
+export interface IIDGenerator {
+  generate(): string;
+}
+export interface IAuthenticator {
+  generateToken(input: AuthenticationData): string;
+  getData(token: string): AuthenticationData;
+}
