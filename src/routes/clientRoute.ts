c7f377c9c83821a5b814b8bf303b62eeb6fb1feb
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2023-08-16 10:09:55 -0300

    feat: rota de cadastro de usuário

---
+++ src/routes/clientRoute.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { ClientController } from "../controller/ClientController";
+import { ClientDatabase } from "../data/ClientDatabase";
+import { ClientBusiness } from "../business/ClientBusiness";
+import { IdGenerator } from "../services/IdGenerator";
+import { HashManager } from "../services/HashManager";
+import { Authenticator } from "../services/Authenticator";
+
+export const clientRouter = express.Router();
+
+const idGenerator = new IdGenerator()
+const hashManager = new HashManager()
+const authenticator = new Authenticator()
+
+const clientDatabase = new ClientDatabase()
+const clientBusiness = new ClientBusiness(clientDatabase,idGenerator, hashManager, authenticator)
+const clientController = new ClientController(clientBusiness)
+
+clientRouter.post("/signup",(res, req) =>  clientController.signup(res, req));
