ac196b5de11719909a24d5682e5b78a3f9a90f82
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2023-08-22 15:23:18 -0300

    Refactor: Fetarondo o app e ajustando variavel de ambiente

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,7 @@
-import dotenv from "dotenv";
-import { AddressInfo } from "net";
-import express from "express";
-import cors from "cors"
+import { app } from "./app"
 import { clientRouter } from "./routes/clientRoute";
 import { clientTypeService } from "./routes/clientTypeService";
 
-dotenv.config();
-const app = express();
-app.use(cors());
-
-app.use(express.json());
 
 app.use("/cliente",clientRouter);
 app.use("/service", clientTypeService)
-
-const server = app.listen(process.env.PORT || 3000, () => {
-  if (server) {
-    const address = server.address() as AddressInfo;
-    console.log(`Servidor rodando em http://localhost:${address.port}`);
-  } else {
-    console.error(`Falha ao rodar o servidor.`);
-  }
-});
