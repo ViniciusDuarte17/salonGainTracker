1dfe05e3025e587a9de60a7ef3e906f33829488c
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2023-10-06 17:51:09 -0300

    feat: deploy na vercel

--- src/repository/clientServiceRepository.ts
+++ src/repository/clientServiceRepository.ts
@@ -1,8 +1,8 @@
-import { CombineTipeService, ItypeService, ItypeServiceDTO } from "../model/typeService";
+import { ItypeService, ItypeServiceDTO } from "../model/typeService";
 
 
 export interface ITypeServiceRepository {
-    createClientService (inputService: ItypeService): Promise<void>
-    getClientService(idClient: string): Promise<ItypeService[]>
-    updateTypeService(id: string, typeService: ItypeServiceDTO): Promise<void>
+  createClientService(inputService: ItypeService): Promise<void>;
+  getClientService(idClient: string): Promise<ItypeService[]>;
+  updateTypeService(id: string, typeService: ItypeServiceDTO): Promise<void>;
 }
